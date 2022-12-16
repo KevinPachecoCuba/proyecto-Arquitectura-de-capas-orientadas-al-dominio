0f53f3dced91612e4c20de50294adeed0dab067f
prueba <kpachecocuba@gmail.com>
2022-12-16 10:19:02 -0500

    Agregar archivos de proyecto.

---
+++ prueba/Controllers/EstudianteController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using prueba.Models;
+using prueba.Models.ViewModels;
+
+namespace prueba.Controllers
+{
+    public class EstudianteController : Controller
+    {
+        // GET: Estudiante
+        public ActionResult estudiante()
+
+        {
+            List<vmestudiante> lst;
+            using (colegioEntities1 db = new colegioEntities1())
+            {
+                lst = (from d in db.estudiante
+                       select new vmestudiante
+                       {
+                           idestudiante = d.idestudiante,
+                           dni = d.dni,
+                           nombre = d.nombre,
+                           ape_pat = d.ape_pat,
+                           ape_mat = d.ape_mat,
+                           direccion = d.direccion,
+                           celular = d.celular,
+                           correo = d.correo,
+                           fec_nac = (DateTime)d.fec_nac,
+
+                       }).ToList();
+
+
+            }
+
+            return View(lst);
+        }
+
+        public ActionResult NuevoEstudiante()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult NuevoEstudiante(vmestudiante model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (colegioEntities1 db = new colegioEntities1())
+
+                    {
+                        var otabla = new estudiante();
+
+                        otabla.idestudiante = model.idestudiante;
+                        otabla.dni = model.dni;
+                        otabla.nombre = model.nombre;
+                        otabla.ape_pat = model.ape_pat;
+                        otabla.ape_mat = model.ape_mat;
+                        otabla.direccion = model.direccion;
+                        otabla.celular = model.celular;
+                        otabla.correo = model.correo;
+                        otabla.fec_nac = model.fec_nac;
+
+                    }
+
+                    return Redirect("~/estudiante/estudiante");
+                }
+
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+    }
+}
+   
---
+++ prueba/Models/ViewModels/vmestudiante.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace prueba.Models.ViewModels
+{
+    public class vmestudiante
+    {
+        public int idestudiante { get; set; }
+
+        [Required]
+        [StringLength(8)]
+        [Display(Name = "dni")]
+        public String dni { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public String nombre { get; set; }
+
+        public String ape_pat { get; set; }
+        public String ape_mat { get; set; }
+        public String direccion { get; set; }
+        public String celular { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Correo electtronico")]
+        public String correo { get; set; }
+        public DateTime fec_nac { get; set; }
+
+    }
+}
